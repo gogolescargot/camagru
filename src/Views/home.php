e213302dcb1936ec9025b19f60295c38a3ece822
gogolescargot <gauthiergalon@pm.me>
2025-06-11 19:23:32 +0200

    Add Database Connection

--- src/Views/home.php
+++ src/Views/home.php
@@ -8,3 +8,18 @@
     <h1>Welcome to Home</h1>
 </body>
 </html>
+
+<?php
+$host = getenv('MYSQL_HOST');
+$dbname = getenv('MYSQL_DATABASE');
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected to the database successfully!";
+}
+catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
